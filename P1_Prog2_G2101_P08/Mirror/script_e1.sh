a9a1fe093b9fea9d4fc384b49bb1adf996fea1ae
Martín <mssignorini@gmail.com>
2019-02-20 19:07:23 +0100

    Update script_e1.sh
    - Clear
    - Flags

--- P1_Prog2_G2101_P08/Mirror/script_e1.sh
+++ P1_Prog2_G2101_P08/Mirror/script_e1.sh
@@ -4,12 +4,17 @@
 
 #################################
 #! /bin/bash
-gcc -c node.c
-gcc -c p1_e1.c
-gcc -o p1_e1 p1_e1.o node.o
-./p1_e1
+p1_e1 clear
+
+gcc -g -Wall -ansi -pedantic -c node.c
+gcc -g -Wall -ansi -pedantic -c p1_e1.c
+gcc -g -Wall -ansi -pedantic -o p1_e1 p1_e1.o node.o
+
+rm -rf *.o
+
+valgrind --leak-check=full ./p1_e1
 #################################
 
 # Usando valgrind
-gcc -g node.c p1_e1.c -o p1_e1
-valgrind --leak-check=full ./p1_e1
+# gcc -g node.c p1_e1.c -o p1_e1
+# valgrind --leak-check=full ./p1_e1
