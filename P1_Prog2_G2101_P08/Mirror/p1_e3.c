1eb443ac508b865e3fa572fc8ad1c4dfba6c4a23
Martín <mssignorini@gmail.com>
2019-02-20 18:58:30 +0100

    Comments

--- P1_Prog2_G2101_P08/Mirror/p1_e3.c
+++ P1_Prog2_G2101_P08/Mirror/p1_e3.c
@@ -1,17 +1,16 @@
 /* 
- * File:   main.c
- * Author: e400156
+ * MAIN EJERCICIO 3
+ * Nombre: Martín Sánchez
  *
- * Created on 5 de febrero de 2019, 13:37
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #include "graph.h"
 
-/*
- */
+extern int errno;
 
 void main_destroy(int salida, Node *n1, Node *n2, Graph *pg);
 
@@ -23,13 +22,13 @@ int main(int argc, char** argv) {
     
     FILE *f = NULL;
     
-    // INICIAR GRAPH
+    /* INICIAR GRAPH */
     g = graph_ini();
     if(g == NULL) return EXIT_FAILURE;
     
     
-    // Abrir FILE
-    // f = fopen("g1.txt", "r");
+    /* Abrir FILE */
+    /* f = fopen("g1.txt", "r"); */
     f = fopen(argv[1], "r");
     if(f == NULL) main_destroy(EXIT_FAILURE, n1, n2, g);
     
@@ -46,7 +45,7 @@ int main(int argc, char** argv) {
  *  Usar como argumentos los elementos a liberar
  */
 void main_destroy (int salida, Node *n1, Node *n2, Graph* pg) {
-    // Destruir todos los elementos del heap, pasados como argumentos
+    /* Destruir todos los elementos del heap, pasados como argumentos */
     node_destroy(n1);
     n1 = NULL;
     node_destroy(n2);
