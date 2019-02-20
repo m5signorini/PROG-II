1df307accf8961d1190ce21d5233e4c46dd94d51
Martín <mssignorini@gmail.com>
2019-02-20 18:38:41 +0100

    Comments

--- P1_Prog2_G2101_P08/Mirror/p1_e2.c
+++ P1_Prog2_G2101_P08/Mirror/p1_e2.c
@@ -1,14 +1,16 @@
 /* 
- * File:   main.c
- * Author: e400156
+ * MAIN EJERCICIO 2
+ * Nombre: Martín Sánchez
  *
- * Created on 5 de febrero de 2019, 13:37
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #include "graph.h"
+
+extern int errno;
 
 /*
     -Inicialice dos nodos. El primero con nombre “first” e id 111 y
@@ -31,29 +33,29 @@ int main(int argc, char** argv) {
     Node *n1 = NULL;
     Node *n2 = NULL;
     Graph *g = NULL;
-    // Variable auxiliar para el checkeo de retornos y comparaciones
+    /* Variable auxiliar para el checkeo de retornos y comparaciones */
     int id1, id2, aux;
     Status flag;
     
-    // INICIAR
+    /* INICIAR */
     n1 = node_ini();
     if(n1 == NULL) return EXIT_FAILURE;
     n2 = node_ini();
     if(n2 == NULL) main_destroy(EXIT_FAILURE, n1, n2, g);
     
-    // SET NODE 1
+    /* SET NODE 1 */
     node_setId(n1, 111);
     node_setName(n1, "first");
     
-    // SET NODE 2
+    /* SET NODE 2 */
     node_setId(n2, 222);
     node_setName(n2, "second");
     
-    // INICIAR GRAFO
+    /* INICIAR GRAFO */
     g = graph_ini();
     if(g == NULL) main_destroy(EXIT_FAILURE, n1, n2, g);
     
-    // INSERCIONES
+    /* INSERCIONES */
     fprintf(stdout, "Insertando nodo 1...");
     flag = graph_insertNode(g, n1);
     fprintf(stdout, "resultado...%d\n", flag);
@@ -62,7 +64,7 @@ int main(int argc, char** argv) {
     flag = graph_insertNode(g, n2);
     fprintf(stdout, "resultado...%d\n", flag);
     
-    // EDGE
+    /* EDGE */
     id1 = node_getId(n1);
     if(id1 == -1) main_destroy(EXIT_FAILURE, n1, n2, g);
     id2 = node_getId(n2);
@@ -73,7 +75,7 @@ int main(int argc, char** argv) {
     if(flag == ERROR) main_destroy(EXIT_FAILURE, n1, n2, g);
     
     
-    // CHECK CONNECTION
+    /* CHECK CONNECTION */
     fprintf(stdout, "Conectados nodo 1 y nodo 2? ");
     if(graph_areConnected(g, id1, id2) == TRUE) {
         fprintf(stdout, "Si\n");
@@ -90,12 +92,12 @@ int main(int argc, char** argv) {
         fprintf(stdout, "No\n");
     }
     
-    // INSERTAR NODO 2
+    /* INSERTAR NODO 2 */
     fprintf(stdout, "Insertando nodo 2...");
     flag = graph_insertNode(g, n2);
     fprintf(stdout, "resultado...%d\n", flag);
     
-    // PRINT GRAFO
+    /* PRINT GRAFO */
     fprintf(stdout, "GRAFO\n");
     aux = graph_print(stdout, g);
     if(aux == -1) main_destroy(EXIT_FAILURE, n1, n2, g);
@@ -109,7 +111,7 @@ int main(int argc, char** argv) {
  *  Usar como argumentos los elementos a liberar
  */
 void main_destroy (int salida, Node *n1, Node *n2, Graph* pg) {
-    // Destruir todos los elementos del heap, pasados como argumentos
+    /* Destruir todos los elementos del heap, pasados como argumentos */
     node_destroy(n1);
     n1 = NULL;
     node_destroy(n2);
