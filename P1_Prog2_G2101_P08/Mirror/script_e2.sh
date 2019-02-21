9e4739164bb56b2aa2dfb9d9118d774790008fea
Martín <mssignorini@gmail.com>
2019-02-21 17:33:35 +0100

    Last update

--- P1_Prog2_G2101_P08/Mirror/script_e2.sh
+++ P1_Prog2_G2101_P08/Mirror/script_e2.sh
@@ -4,9 +4,11 @@
 
 #################################
 #! /bin/bash
+p1_e2 clear
 gcc -g -Wall -pedantic -ansi -std=c99 -c node.c
 gcc -g -Wall -pedantic -ansi -std=c99 -c graph.c
 gcc -g -Wall -pedantic -ansi -std=c99 -c p1_e2.c
 gcc -g -Wall -pedantic -ansi -std=c99 -o p1_e2 p1_e2.o node.o graph.o
+rm -rf *.o
 valgrind --leak-check=full ./p1_e2
 #################################
