e94296cdb63cf41de2b8443b1b4d10857b4da45a
Martín <mssignorini@gmail.com>
2019-02-21 19:20:07 +0100

    Update script_e3.sh

--- P1_Prog2_G2101_P08/Mirror/script_e3.sh
+++ P1_Prog2_G2101_P08/Mirror/script_e3.sh
@@ -4,11 +4,13 @@
 
 #################################
 #! /bin/bash
-p1_e3 clear
+
 gcc -g -Wall -pedantic -ansi -std=c99 -c node.c
 gcc -g -Wall -pedantic -ansi -std=c99 -c graph.c
 gcc -g -Wall -pedantic -ansi -std=c99 -c p1_e3.c
 gcc -g -Wall -pedantic -ansi -std=c99 -o p1_e3 p1_e3.o node.o graph.o
+
 rm -rf *.o
+
 valgrind --leak-check=full ./p1_e3 g1.txt
 #################################
