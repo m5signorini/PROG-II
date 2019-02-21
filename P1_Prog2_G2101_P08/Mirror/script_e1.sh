b90dea725ed9e64d1b9cf50cdce61a76ab4e5de1
Martín <mssignorini@gmail.com>
2019-02-21 17:30:58 +0100

    Last Update

--- P1_Prog2_G2101_P08/Mirror/script_e1.sh
+++ P1_Prog2_G2101_P08/Mirror/script_e1.sh
@@ -6,15 +6,11 @@
 #! /bin/bash
 p1_e1 clear
 
-gcc -g -Wall -ansi -pedantic -c node.c
-gcc -g -Wall -ansi -pedantic -c p1_e1.c
-gcc -g -Wall -ansi -pedantic -o p1_e1 p1_e1.o node.o
+gcc -g -Wall -pedantic -ansi -std=c99 -c node.c
+gcc -g -Wall -pedantic -ansi -std=c99 -c p1_e1.c
+gcc -g -Wall -pedantic -ansi -std=c99 -o p1_e1 p1_e1.o node.o
 
 rm -rf *.o
 
 valgrind --leak-check=full ./p1_e1
 #################################
-
-# Usando valgrind
-# gcc -g node.c p1_e1.c -o p1_e1
-# valgrind --leak-check=full ./p1_e1
