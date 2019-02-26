886f90f1a409e8ff09c6069eda1c26c365be6494
Martín <mssignorini@gmail.com>
2019-02-26 14:02:08 +0100

    Entrega 2

---
+++ P2_Prog2_G2101_P08/src int/p2_e2.c
@@ -0,0 +1,143 @@
+/* 
+ * 
+ * P2 - Ejercicio 2
+ *
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "stack_elestack.h"
+/*
+ * 
+ */
+
+void main_exit(int mess, Stack *st, EleStack * ele, int* n);
+double pila_media(Stack *st);
+
+int main(int argc, char** argv) {
+    
+    Stack * pila = NULL;
+    EleStack * element = NULL;
+    int * pe = NULL;
+    int aux = 0, n = 0;
+    double media;
+    Status flag;
+    
+    //INICIALIZACION
+    pila = stack_ini();
+    if(pila == NULL) return EXIT_FAILURE;
+    
+    element = EleStack_ini();
+    if(element == NULL) main_exit(EXIT_FAILURE, pila, element, pe);
+    
+    pe = (int*)malloc(sizeof(int));
+    if(pe == NULL) main_exit(EXIT_FAILURE, pila, element, pe);
+    
+    n = atoi(argv[1]);
+    
+    //INSERT: 0 a n
+    
+    for(aux = 0; aux <= n; aux++) {
+        // set pe a aux
+        *pe = aux;
+        
+        // set info a pe
+        flag = EleStack_setInfo(element, (void*)pe);
+        if(flag == ERROR) main_exit(EXIT_FAILURE, pila, element, pe);
+        
+        // push element a pila
+        // si la pila se llena se devuelve error y se finaliza
+        flag = stack_push(pila, element);
+        if(flag == ERROR) main_exit(EXIT_FAILURE, pila, element, pe);
+    }
+    
+    //FREE ELEMENT
+    EleStack_destroy(element);
+    element = NULL;
+    
+    //PRINT ANTES DE LA FUNCION
+    fprintf(stdout, "Pila antes de la llamada a la función:\n");
+    aux = stack_print(stdout, pila);
+    if(aux == -1) main_exit(EXIT_FAILURE, pila, element, pe);
+    
+    //CALCULAR MEDIA
+    media = pila_media(pila);
+    fprintf(stdout, "La media es %lf\n", media);
+    
+    //PRINT DESPUES DE LA FUNCION
+    fprintf(stdout, "Pila después de la llamada a la función:\n");
+    aux = stack_print(stdout, pila);
+    if(aux == -1) main_exit(EXIT_FAILURE, pila, element, pe);
+    /*
+    */
+    
+    main_exit(EXIT_SUCCESS, pila, element, pe);
+    return EXIT_FAILURE;
+}
+
+void main_exit(int mess, Stack * st, EleStack * ele, int* n) {
+    stack_destroy(st);
+    st = NULL;
+    EleStack_destroy(ele);
+    ele = NULL;
+    free(n);
+    n = NULL;
+    
+    exit(mess);
+}
+
+
+double pila_media(Stack *st) {
+    if(st == NULL) return 0;
+    
+    EleStack *info = NULL;
+    Stack *paux = NULL;
+    int *valor = NULL;
+    int tam = 0;
+    double retorno = 0;
+    
+    paux = stack_ini();
+    if(paux == NULL) return 0;
+    
+    while(stack_isEmpty(st) == FALSE) {
+        // Pop st y push paux
+        info = stack_pop(st);
+        if(stack_push(paux, info) == ERROR) {
+            // Restablecer st si push = ERROR
+            stack_push(st, info);
+            EleStack_destroy(info);
+            while(stack_isEmpty(paux) == FALSE) {
+                // Asumimos que tras k extracciones se realizaran
+                // k inserciones sin problemas
+                info = stack_pop(paux);
+                stack_push(st, info);
+                EleStack_destroy(info);
+            }
+            // Restornamos
+            stack_destroy(paux);
+            return 0;
+        }
+        // Aumentamos tam, sumamos valor y lo liberamos
+        valor = (int*)EleStack_getInfo(info);
+        tam++;
+        retorno += *valor;
+        // getInfo devuelve copia - liberar
+        free(valor);
+        EleStack_destroy(info);
+    }
+    
+    while(stack_isEmpty(paux) == FALSE) {
+        info = stack_pop(paux);
+        // Asumimos que tras k extracciones se realizaran
+        // k inserciones sin problemas
+        stack_push(st, info);
+        EleStack_destroy(info);
+    }
+    stack_destroy(paux);
+    retorno = retorno / tam;
+    
+    return retorno;
+}
+
---
+++ P2_Prog2_G2101_P08/src int/stack_elestack.c
@@ -0,0 +1,127 @@
+/*
+ * ESTRUCTURA - STACK
+ * Nombre: Martín Sánchez
+ * 
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include "stack_elestack.h"
+
+#define MAXSTACK 1024
+
+extern int errno;
+
+/* top se inicializará a -1, por lo tanto
+ * representa el indice del ultimo item
+ * top = cantidad  de items - 1
+ */
+
+struct _Stack {
+    EleStack * item[MAXSTACK];
+    int top;
+};
+
+
+Stack * stack_ini() {
+    Stack * st = NULL;
+    int i;
+    
+    st = (Stack*) malloc(sizeof(Stack));
+    if(st == NULL) {
+        fprintf(stderr, "Valor de errno: %d\n", errno);
+        fprintf(stderr, "Mensaje de errno: %s\n", strerror(errno));
+        return NULL;
+    }
+    
+    // inicializamos a NULL
+    for(i = 0; i < MAXSTACK; i++) {
+        st->item[i] = NULL;
+    }
+    
+    st->top = -1;
+    return st;
+}
+
+void stack_destroy(Stack *st) {
+    if(st == NULL) return;
+    int i;
+    
+    // i: 0 to top+1 (top empieza en -1)
+    for(i = 0; i <= st->top; i++) {
+        EleStack_destroy(st->item[i]);
+    }
+    
+    free(st);
+    return;
+}
+
+
+Status stack_push(Stack *st, const EleStack *ele) {
+    if(st == NULL || ele == NULL) return ERROR;
+    if(stack_isFull(st) == TRUE) return ERROR;
+    
+    EleStack * eleCpy = NULL;
+    
+    eleCpy = EleStack_copy(ele);
+    if(eleCpy == NULL) return ERROR;
+    
+    st->top++;
+    st->item[st->top] = eleCpy;
+    return OK;
+}
+
+
+// No crea copia, redirecciona y saca el item
+EleStack * stack_pop(Stack *st) {
+    if(st == NULL || stack_isEmpty(st) == TRUE) return  NULL;
+    
+    EleStack *elePop = NULL;
+    
+    elePop = st->item[st->top];
+    if(elePop == NULL) return NULL;
+    
+    st->item[st->top] = NULL;
+    st->top--;
+    // Recordar liberar el retorno tras esta funcion
+    return elePop;
+}
+
+
+Bool stack_isEmpty(const Stack *st) {
+    if(st == NULL) return FALSE;
+    if(st->top == -1) return TRUE;
+    return FALSE;
+}
+
+Bool stack_isFull(const Stack *st) {
+    if(st == NULL) return FALSE;
+    if(st->top + 1 == MAXSTACK) return TRUE;
+    return FALSE;
+}
+
+
+int stack_print(FILE* f, const Stack* st) {
+    if(f == NULL || st == NULL) return -1;
+    
+    int nbytes = 0;
+    int i, aux;
+    
+    // Por cada elemento de la pila
+    for(i = st->top; i > -1; i--) {
+        aux = EleStack_print(f, st->item[i]);
+        if(aux == -1) return -1;
+        
+        nbytes += aux;
+        nbytes += fprintf(f, "\n");
+        if(ferror(f)) {
+            fprintf(stderr, "Error al imprimir\n");
+            return -1;
+        }
+    }
+    
+    return nbytes;
+}
