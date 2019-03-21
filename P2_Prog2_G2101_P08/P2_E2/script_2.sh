89494d3c7fba9e7669cc5f14885f56b048a91644
Martín <mssignorini@gmail.com>
2019-03-21 18:15:45 +0100

    Update and rename script_2_v2.sh to script_2.sh

--- P2_Prog2_G2101_P08/P2_E2/script_2.sh
+++ P2_Prog2_G2101_P08/P2_E2/script_2.sh
@@ -2,8 +2,8 @@
 
 gcc -g -Wall -ansi -std=c99 -pedantic -c stack_elestack.c
 gcc -g -Wall -ansi -std=c99 -pedantic -c elestack-int.c
-gcc -g -Wall -ansi -std=c99 -pedantic -c p2_e2_v2.c
-gcc -g -Wall -ansi -std=c99 -pedantic -o p2_e2 stack_elestack.o elestack-int.o p2_e2_v2.o
+gcc -g -Wall -ansi -std=c99 -pedantic -c p2_e2.c
+gcc -g -Wall -ansi -std=c99 -pedantic -o p2_e2 stack_elestack.o elestack-int.o p2_e2.o
 
 rm -rf *.o
 
